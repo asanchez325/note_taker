fe69c7e74746b30671218f54d7ef948475c71d82
Imperial Design <amsanchez@knox.edu>
2020-10-11 20:58:19 -0500

    add routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log("Server is running on" + PORT));
