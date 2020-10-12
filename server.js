27b44ae1ffa9cd849f179f6fadabaacce8f78b48
Imperial Design <amsanchez@knox.edu>
2020-10-11 21:57:00 -0500

    add notes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const fs = require("fs");
 const path = require('path');
 
 const app = express ();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
