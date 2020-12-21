7da87089d2a18ffe64cd7cda1d1516a87d555481
Imperial Design <amsanchez@knox.edu>
2020-12-21 06:40:25 -0600

    transition router to app

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const fs = require("fs");
-const path = require('path');
-const { db } = require('./db/db');
-
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express ();
 
+
+
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use("./apiRoutes");
-app.use("./htmlRoutes");
+
+require("./routes/apiRoutes/noteRoutes")(app);
+require("./routes/htmlRoutes/index")(app);
 
 
 app.listen(PORT, () => {
