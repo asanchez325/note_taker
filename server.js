272c5435eb69898d8466a1a73cd50cd898d66cb9
Imperial Design <amsanchez@knox.edu>
2020-12-21 00:44:19 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const fs = require("fs");
 const path = require('path');
 
+const PORT = process.env.PORT || 3001;
 const app = express ();
-var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -11,6 +11,6 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});  
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
